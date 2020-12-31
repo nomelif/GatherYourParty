c3d95067bc75524369cd698ed77545b811efde1a
Théo Friberg <fsmnarmosta@posteo.net>
2020-12-31 20:27:52 +0200

    Added four followers, coded their animations and motion

--- 8-way-motion.gd
+++ 8-way-motion.gd
@@ -7,6 +7,8 @@ var direction = 'down'
 var positions = []
 var directions = []
 var position_index = 0
+var sprites = ['Sorceress', 'Protector', 'Healer']
+var y_order = []
 
 func get_input():
 	velocity = Vector2()
@@ -33,14 +35,16 @@ func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
 	
-	if len(positions) == 20:
+	if len(positions) == 60:
 		if velocity == Vector2(0, 0):
-			get_node('../SorceressSprite').play(directions[(position_index + 1) % 20] + "_idle")
+			for i in range(3):
+				get_node('../'+sprites[i]+'Sprite').play(directions[(position_index + 1 + i*20) % 60] + "_idle")
 		else:
-			get_node('../SorceressSprite').play(directions[(position_index + 1) % 20])
+			for i in range(3):
+				get_node('../'+sprites[i]+'Sprite').play(directions[(position_index + 1 + i*20) % 60])
 	
 	if not velocity == Vector2(0, 0):
-		if len(positions) < 20:
+		if len(positions) < 60:
 			positions.append(position)
 			directions.append(direction)
 			position_index += 1
@@ -49,11 +53,14 @@ func _physics_process(delta):
 			positions[position_index] = position
 			directions[position_index] = direction
 			position_index += 1
-			position_index %= 20
-			if len(positions) == 20:
-				get_node('../SorceressSprite').position = positions[(position_index + 1) % 20]
-				if get_node('../SorceressSprite').position.y > position.y:
-					get_node('../SorceressSprite').z_index = z_index + 1
-				else:
-					get_node('../SorceressSprite').z_index = z_index - 1
+			position_index %= 60
+			if len(positions) == 60:
+				for i in range(3):
+					get_node('../'+sprites[i]+'Sprite').position = positions[(position_index + 1 + i*20) % 60]
+				y_order = [['FighterSprite', position.y]]
+				for i in range(3):
+					y_order.append(['../'+sprites[i]+'Sprite', get_node('../'+sprites[i]+'Sprite').position.y])
+				y_order.sort()
+				for i in range(4):
+					get_node(y_order[i][0]).z_index = 10 - i
 	
