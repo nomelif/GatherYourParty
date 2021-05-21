7030a9f07f3829d01d2a22ed4cf2aa389357598a
Théo Friberg <fsmnarmosta@posteo.net>
2021-05-21 19:58:58 +0300

    Added camera motion to ruins stage

---
+++ Camera2D.gd
@@ -0,0 +1,24 @@
+extends Camera2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var places = {"Hub": [-400, -200, 4000, 3800],
+			  "Ruins": [3800, -200, 7200, 2400]}
+
+func move_to(place):
+	self.limit_left = places[place][0]
+	self.limit_top = places[place][1]
+	self.limit_right = places[place][2]
+	self.limit_bottom = places[place][3]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
