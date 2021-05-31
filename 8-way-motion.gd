f1f3af5d5a03fdf945090cfc1085c22d90aa914f
Théo Friberg <fsmnarmosta@posteo.net>
2021-05-31 14:13:05 +0300

    Refactored motion script

--- 8-way-motion.gd
+++ 8-way-motion.gd
@@ -2,14 +2,16 @@ extends KinematicBody2D
 
 export (int) var speed = 350
 export (float) var run_multiplier = 1.5
+export (int) var people_qty = 4
 
 var velocity = Vector2()
 var direction = 'down'
 var positions = []
 var directions = []
 var position_index = 0
-var sprites = ['Sorceress', 'Protector', 'Healer']
-var y_order = []
+var sprites = ['Fighter', 'Protector', 'Sorceress', 'Healer']
+var steps = 20
+var total_steps = steps*people_qty
 
 func get_input():
 	velocity = Vector2()
@@ -25,11 +27,6 @@ func get_input():
 	if Input.is_action_pressed('up'):
 		direction = 'up'
 		velocity.y -= 1
-	if velocity != Vector2(0, 0):
-		$FighterSprite.play(direction)
-	else:
-		$FighterSprite.play(direction + "_idle")
-	# get_node('../SorceressSprite').play(animation_choice)
 	var effective_speed = speed
 	if Input.is_action_pressed('run'):
 		effective_speed *= run_multiplier
@@ -38,27 +35,23 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
-	
-	if len(positions) == 60:
-		if velocity == Vector2(0, 0):
-			for i in range(3):
-				get_node('../'+sprites[i]+'Sprite').play(directions[(position_index + 1 + i*20) % 60] + "_idle")
-		else:
-			for i in range(3):
-				get_node('../'+sprites[i]+'Sprite').play(directions[(position_index + 1 + i*20) % 60])
-	
 	if not velocity == Vector2(0, 0):
-		if len(positions) < 60:
+		if len(positions) < total_steps:
 			positions.append(position)
 			directions.append(direction)
-			position_index += 1
-			position_index %= 20
 		else:
 			positions[position_index] = position
 			directions[position_index] = direction
-			position_index += 1
-			position_index %= 60
-			if len(positions) == 60:
-				for i in range(3):
-					get_node('../'+sprites[i]+'Sprite').position = positions[(position_index + 1 + i*20) % 60]
+		position_index += 1
+		position_index %= total_steps
+	if len(positions) == total_steps:
+		if velocity == Vector2(0, 0):
+			for i in range(people_qty):
+				get_node('../'+sprites[i]+'Sprite').play(directions[(position_index - i*steps - 1) % total_steps] + "_idle")
+		else:
+			for i in range(people_qty):
+				get_node('../'+sprites[i]+'Sprite').play(directions[(position_index - i*steps - 1) % total_steps])
+			if len(positions) == total_steps:
+				for i in range(people_qty):
+					get_node('../'+sprites[i]+'Sprite').position = positions[(position_index - i*steps - 1) % total_steps]
 	get_node('/root/Main/Camera2D').position = position;	
