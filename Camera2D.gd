116a9efc2d3342361017d5e4bffbf65fa6a984d2
Théo Friberg <fsmnarmosta@posteo.net>
2021-06-20 19:09:56 +0300

    Added camera trigger for meadow

--- Camera2D.gd
+++ Camera2D.gd
@@ -7,7 +7,8 @@ extends Camera2D
 
 var places = {"Hub": [-400, -200, 4000, 3800],
 			  "Ruins": [3800, -200, 7200, 2300],
-			  "Swamp": [3600, 2300, 7200, 4800]}
+			  "Swamp": [3600, 2300, 7200, 4800],
+			  "Meadow": [-394.311, 3403.773, 3983.097, 6183.847]}
 
 func move_to(place):
 	self.limit_left = places[place][0]
