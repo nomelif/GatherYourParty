075fee1a2d4c9b867e19d33d7bd0df0c4a1ab056
Théo Friberg <fsmnarmosta@posteo.net>
2021-08-19 11:12:46 +0300

    Added scroll sound

--- Sprite scenes/Scroll.gd
+++ Sprite scenes/Scroll.gd
@@ -17,8 +17,15 @@ func _ready():
 
 
 func _on_Area2D_body_entered(body):
-	if body.name == "PlayerCharacter":
+	if body.name == "PlayerCharacter" && visible:
 		body.queue_animations([["../../"+body.front_peep()+"Bubble", "Creation"],
 							   ["../../"+body.front_peep()+"Bubble", "Exclamation"],
-							   ["../../"+body.front_peep()+"Bubble", "Deletion"]])
-		self.get_parent().remove_child(self)
+							   ["../../"+body.front_peep()+"Bubble", "Deletion"],
+							   [null, funcref(self, "post_animation")]])
+
+
+func post_animation():
+	$AudioStreamPlayer2D.play()
+	visible = false
+	yield(get_tree().create_timer(8.333), "timeout")
+	self.get_parent().remove_child(self)
