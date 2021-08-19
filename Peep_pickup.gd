789812a4c98e1df29c7ef08ee4a1300efff0c71b
Théo Friberg <fsmnarmosta@posteo.net>
2021-08-19 11:32:47 +0300

    Added peep pickup sounds

--- Peep_pickup.gd
+++ Peep_pickup.gd
@@ -19,7 +19,7 @@ func _ready():
 #	pass
 
 func _on_body_entered(body):
-	if body.name == "PlayerCharacter":
+	if body.name == "PlayerCharacter" && visible:
 		body.queue_animations(dialogue)
 		player = body
 		body.queue_animations([[null, funcref(self, "cleanup")]])
@@ -28,4 +28,7 @@ func cleanup():
 	player.people_qty += 1
 	get_node('../'+character+'Sprite').visible = true
 	player.cutscene = false
+	$AudioStreamPlayer2D.play()
+	visible = false
+	yield(get_tree().create_timer(4.261), "timeout")
 	get_parent().remove_child(self)
