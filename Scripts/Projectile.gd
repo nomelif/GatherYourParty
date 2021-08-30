11deb35bc4f52b0ae0f21e52fd7758ca6e015212
Théo Friberg <fsmnarmosta@posteo.net>
2021-08-30 14:03:09 +0300

    Cleaned up machine comments from Projectile

--- Scripts/Projectile.gd
+++ Scripts/Projectile.gd
@@ -1,10 +1,5 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var target = self
 var speed = 16000
 var colors = {
@@ -16,12 +11,6 @@ var colors = {
 
 var parent = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if target.global_position == global_position:
 		return
