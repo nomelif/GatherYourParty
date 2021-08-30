189377a6e2392b5f65d4b545f07879b82f0e3613
Théo Friberg <fsmnarmosta@posteo.net>
2021-08-30 13:47:43 +0300

    Cleaned up & renamed Boss Play Area Transform

--- Scripts/Boss Play Area Transform.gd
+++ Scripts/Boss Play Area Transform.gd
@@ -1,25 +1,13 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	position.x = get_viewport().size.x / 2 - $Background.rect_size.x / 2;
 	position.y = get_viewport().size.y / 2 - $Background.rect_size.y / 2;
 	get_node("../Black").rect_size.x = get_viewport().size.x;
 	get_node("../Black").rect_size.y = get_viewport().size.y;
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x = get_viewport().size.x / 2 - $Background.rect_size.x / 2;
 	position.y = get_viewport().size.y / 2 - $Background.rect_size.y / 2;
 	get_node("../Black").rect_size.x = get_viewport().size.x;
 	get_node("../Black").rect_size.y = get_viewport().size.y;
-	#$ColorRect.rect_size.x = get_viewport().size.x;
-	#$ColorRect.rect_size.y = get_viewport().size.y;
