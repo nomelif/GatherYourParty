4131e39d22160a52c02ca0cc01c1fdfe24fda875
Théo Friberg <fsmnarmosta@posteo.net>
2021-08-30 13:39:15 +0300

    Cleaned up & renamed Main Menu Position

--- Scripts/Main Menu Position.gd
+++ Scripts/Main Menu Position.gd
@@ -1,19 +1,9 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	position.x = get_viewport().size.x - 172 - 20;
 	position.y = get_viewport().size.y - 400;
-	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x = get_viewport().size.x - 172 - 20;
 	position.y = get_viewport().size.y - 400;
