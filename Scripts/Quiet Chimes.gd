30cdab57b970a221d7bc929417c341b1164bfa6e
Théo Friberg <fsmnarmosta@posteo.net>
2021-08-30 14:08:44 +0300

    Renamed & cleaned Quiet Chimes

--- Scripts/Quiet Chimes.gd
+++ Scripts/Quiet Chimes.gd
@@ -1,15 +1,9 @@
 extends AudioStreamPlayer
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var rng = RandomNumberGenerator.new()
 export var lower_bound = 4
 export var upper_bound = 20
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	rng.randomize()
 	while true:
@@ -17,8 +11,3 @@ func _ready():
 		play(0)
 		yield(get_tree().create_timer(self.stream.get_length()), "timeout")
 		stop()
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
