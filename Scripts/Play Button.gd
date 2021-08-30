a3503f94f90f6356a8c7f75ff3e9d06a5f867fc9
Théo Friberg <fsmnarmosta@posteo.net>
2021-08-30 13:49:17 +0300

    Cleaned up & renamed Play Button

--- Scripts/Play Button.gd
+++ Scripts/Play Button.gd
@@ -1,20 +1,6 @@
 extends Button
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 export var preserve_save = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _on_button_up():
 	$Pressed.visible = false
