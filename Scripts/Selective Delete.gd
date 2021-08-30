13add8ac84fc14ec5ec987d427c904db58a36b6c
Théo Friberg <fsmnarmosta@posteo.net>
2021-08-30 20:46:33 +0300

    Renamed & cleaned Selective Delete

--- Scripts/Selective Delete.gd
+++ Scripts/Selective Delete.gd
@@ -1,24 +1,14 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 export(String) var required_character;
 export(Array) var success_dialogue;
 
 var in_body = false
 
-# Called when the node enters the scene tree for the first time.
+# Don't force the player to walk out and back in upon changing peeps
+
 func _ready():
 	get_node("../PlayerCharacter").connect("peeps_changed", self, "logic")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_body_entered(body):
 	if body.name == "PlayerCharacter":
@@ -47,11 +37,6 @@ func logic():
 
 func delete_sprite():
 	self.get_parent().remove_child(self)
-
-
-func _on_animation_finished():
-	pass # Replace with function body.
-
 
 func _on_body_exited(body):
 	if body.name == "PlayerCharacter":
