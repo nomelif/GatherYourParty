789f98b82f57665afca15103de671c37ccbdda62
Théo Friberg <fsmnarmosta@posteo.net>
2021-08-31 10:53:48 +0300

    Moved art folders under Visual Art

--- make_atlas.sh
+++ make_atlas.sh
@@ -1,2 +1,2 @@
 #convert -append Original\ Tiles/*png -interpolate Nearest -filter point -resize 400% atlas.png
-montage Original\ Tiles/*png -geometry 200x200+0+0 -interpolate Nearest -filter point atlas.png
+montage Visual\ Art/Original\ Tiles/*png -geometry 200x200+0+0 -interpolate Nearest -filter point Visual\ Art/atlas.png
